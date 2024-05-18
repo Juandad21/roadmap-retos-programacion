3880390f7b67fa882d30b85d2d449bbaa71fd5ab
Juandad21 <Mejia.enriquezj@gmail.com>
2024-05-17 19:07:13 -0500

    Create juandad21.java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/juandad21.java
@@ -0,0 +1,20 @@
+// https://www.java.com/es/
+
+// comentario en una sola linea 
+
+/* este es un comentario
+  en varias lineas. */
+
+/** este es otro comentario para 
+  la documentacion para un un proceso 
+  en concreto para llevar a cabo en la 
+  herramienta javadoc */
+
+int numero_de_goles_TA=30;
+string jugador = "Ronaldo";
+double promedio = 1.2;
+boolean total = true;
+char rendimiento = "s";
+final int numero_de_goles_FC = 200;
+
+systemn.out.printin("¡Hola, java.");
